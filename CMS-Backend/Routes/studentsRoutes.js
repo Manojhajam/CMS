1ffaaa7ee62a44f45bb7ed8e5254e7c3a3efbbdf
Manoj Hajam <manojhajam4@gmail.com>
2025-08-29 17:45:31 +0545

    middleware

--- CMS-Backend/Routes/studentsRoutes.js
+++ CMS-Backend/Routes/studentsRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createStudent } from "../Controllers/studentControllers.js";
+import { checkAuthorization } from "../middleware/checkAuthorization.js";
+
 
 const studentRouter = express.Router()
 
-studentRouter.route("/").post(createStudent)
+studentRouter.route("/").post(checkAuthorization, createStudent)
 
 export default studentRouter;
