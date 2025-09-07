ff1f6d536309275620e2bd6fe2b5b3940787d04e
Manoj Hajam <manojhajam4@gmail.com>
2025-09-07 20:19:20 +0545

    courseDetails updated

--- CMS-Backend/Controllers/commonControllers.js
+++ CMS-Backend/Controllers/commonControllers.js
@@ -1,19 +1,30 @@
 import { courseModel } from "../Model/coureseModel.js";
 
 export const courseDetails = async (req, res) => {
+  try {
     const { courseId } = req.params;
 
-    const course =await courseModel.findById(courseId);
+    const course = await courseModel
+      .findById(courseId)
+      .populate("students", "rollNumber department userId") // populate student docs
+      .populate("facultyId", "name department userId"); // show faculty info
 
     if (!course) {
-        return res.status(403).json({
-            success: false,
-            message: "Course not found!"
-        })
+      return res.status(404).json({
+        success: false,
+        message: "Course not found!"
+      });
     }
 
     res.status(200).json({
-        success: true,
-        data: course
-    })
-}
+      success: true,
+      data: course,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
