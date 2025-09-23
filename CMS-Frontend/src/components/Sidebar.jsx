9492ab62ff5c736c4382c061793ec7a7abe734a3
Manoj Hajam <manojhajam4@gmail.com>
2025-09-23 12:16:17 +0545

    logout button done

--- CMS-Frontend/src/components/Sidebar.jsx
+++ CMS-Frontend/src/components/Sidebar.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router";
+import { AuthContext } from "../context/authContext";
+import { FiLogOut } from "react-icons/fi";
 
 const Sidebar = () => {
+  const {user, setUser} = useContext(AuthContext)
+
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    setUser(null)
+  }
   return (
     <nav className="fixed w-[250px] h-[100vh] bg-gray-200 p-3 flex flex-col justify-between">
       <div>
         <h3 className="text-3xl font-bold mb-4 p-4">CMS</h3>
-        <div className='className="flex flex-col gap-2'>
-          <NavLink to={"/sidebar"}>Dashboard</NavLink>
+        <div className="flex flex-col gap-2">
+          <NavLink
+            className="px-4 py-4 text-lg rounded-lg hover:bg-[#8EA4D2] hover:text-white"
+            to={"/sidebar"}
+          >
+            Dashboard
+          </NavLink>
 
           <NavLink
             className="px-4 py-4 text-lg rounded-lg hover:bg-[#8EA4D2] hover:text-white"
@@ -17,6 +30,13 @@ const Sidebar = () => {
           </NavLink>
         </div>
       </div>
+      <button
+        onClick={handleLogOut}
+        className="flex justify-center items-center p-3 rounded-lg bg-gradient-to-tl bg-pink-500 to-green-500 cursor-pointer hover:bg-red-800"
+      >
+        <FiLogOut />
+        Log Out
+      </button>
     </nav>
   );
 };
