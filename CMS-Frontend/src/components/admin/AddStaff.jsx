d9333c23aa0fe6f581466aa405a89124e13ab75b
Manoj Hajam <manojhajam4@gmail.com>
2025-09-25 06:45:55 +0545

    added card and Modal

--- CMS-Frontend/src/components/admin/AddStaff.jsx
+++ CMS-Frontend/src/components/admin/AddStaff.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '../common/Modal';
 
 const AddStaff = () => {
+const {showModel, setShowModal} = useState()
+
   return (
     <div className="bg-emerald-100 h-screen">
       <div className="bg-white p-4 ">
@@ -8,6 +11,8 @@ const AddStaff = () => {
           Student Management System | Admin Dashboard
         </h1>
       </div>
+        <button className='bg-amber-300 p-2 rounded-lg mt-4' >Add Staff</button>
+      {/* <Modal/> */}
     </div>
   );
 }
---
+++ CMS-Frontend/src/components/common/Card.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+// import Card from './common/Card'
+
+const Card = ({ children, customClass, onclick }) => {
+  return (
+    <div
+      onClick={onclick}
+      className={`px-8 py-5 shadow-lg border border-gray-400  rounded-lg + ${customClass} ${
+        onclick ? "cursor-pointer" : ""
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
